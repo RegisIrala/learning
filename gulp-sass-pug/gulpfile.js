87c9110835d7d5af6ed86941329b6a37ade674bc
Régis Irala <regisirala@hotmail.com>
2018-05-17 16:30:52 -0300

    Getting started with Gulp Sass Pug

---
+++ gulp-sass-pug/gulpfile.js
@@ -0,0 +1,58 @@
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var pug = require('gulp-pug');
+var htmlmin = require("gulp-htmlmin");
+var notify = require("gulp-notify");
+var uglify = require("gulp-uglify");
+var browserSync = require("browser-sync").create();
+
+/* Task compile pug to html */
+gulp.task('pug', function() {
+    return gulp.src('./src/pug/*.pug')
+        .pipe(pug())
+        .pipe(gulp.dest('./dist/'))
+});
+
+/* Task minify html */
+gulp.task('html', function() {
+    return gulp.src('./src/*.html')
+        .pipe(htmlmin({ collapseWhitespace: true }))
+        .pipe(gulp.dest("./dist"))
+        .pipe(browserSync.stream());
+});
+
+/* Task compile sass to css */
+gulp.task('sass', function() {
+    return gulp.src('./src/sass/**/*.sass')
+        .pipe(sass({ outPutStyle: 'compressed' }))
+        .on('error', notify.onError({ title: 'Error SASS', message: '<%= error.message %>' }))
+        .pipe(gulp.dest('./dist/css'))
+        .pipe(browserSync.stream());
+});
+
+/* Task minify js */
+gulp.task('js', function() {
+    return gulp.src('./src/js/*.js')
+        .pipe(uglify())
+        .on('error', notify.onError({ title: 'Error JavaSript', message: '<%= error.message %>' }))
+        .pipe(gulp.dest('./dist/js'))
+        .pipe(browserSync.stream());
+});
+
+/* Task server local */
+gulp.task('watcher', function() {
+    browserSync.init({
+        server: {
+            baseDir: './dist'
+        }
+    });
+
+    /* Watch */
+    gulp.watch('./src/pug/*', ['pug']);
+    gulp.watch('./src/sass/**/*.sass', ['sass']);
+    gulp.watch("./src/components/bootstrap/scss/**/*.scss", ['sass']);
+    gulp.watch("./src/js/**/*.js", ['js']);
+    gulp.watch("./src/*.html", ['html']);
+});
+
+gulp.task('default', ['pug', 'html', 'sass', 'js', 'watcher']);
